107252ae2efdf97ac066cc60503cc5bf86584e80
GCN, the creator <146867533+Nghi-creator@users.noreply.github.com>
2026-03-25 23:18:40 +0700

    [update] add new games to supabase

---
+++ supabase/migrations/20260325161539_add_new_games.sql
@@ -0,0 +1,14 @@
+INSERT INTO games (title, rom_filename, cover_url)
+VALUES 
+  ('Adventure Island 3', 'Adventure Island 3 (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co21z9.png'),
+  ('The Legend of Zelda', 'Legend of Zelda, The (USA) (Rev 1).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co22b7.png'),
+  ('Contra', 'Contra (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2k1k.png'),
+  ('Chip ''n Dale: Rescue Rangers 2', 'Chip ''n Dale - Rescue Rangers 2 (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co22g5.png'),
+  ('Snow Brothers', 'Snow Brothers (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co21a1.png'),
+  ('The Legend of Owlia', 'owlia.nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2lk4.png'),
+  ('Super Mario Bros. 3', 'Super Mario Bros. 3 (USA) (Rev 1).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1wxx.png'),
+  ('Mega Man 2', 'Mega Man 2 (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1xoz.png'),
+  ('Ninja Gaiden', 'Ninja Gaiden (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1u6m.png'),
+  ('Castlevania III: Dracula''s Curse', 'Castlevania III - Dracula''s Curse (USA).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1srp.png'),
+  ('Little Sisyphus', 'little_sisyphus_v1.nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co38qa.png'),
+  ('Aladdin', 'Aladdin (Unl).nes', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1v2a.png');
